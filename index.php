ead31b33e264f5dfe98019767cd82f5e226bcc55
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 16:43:51 -0600

    background 4

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 		<meta name="author" content="Marty Bonacci">
 		<meta name="description" content="About Marty Page"><!--The description appears with the title in search reaults.-->
 		<title>About Marty Page</title>
+	
 	</head>
 	<body>
+		<div style="background-image: src=images/marty-on-mt-taylor.jpg">
+			This text appears in front of the background image.
+		
+
 		<h1>About Marty Bonacci</h1>
-		<img src="images/marty-on-mt-taylor.jpg" width="200" height="auto">
 		<p>More about Marty</p>
 		<p></p>
-
+		</div>
 
 
 
