d820fedbd81a48e021d59d687ef37bfef3d34ee2
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 16:05:15 -0600

    changed name to Marty Bonacci

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta name="author" content="Marty B">
+		<meta name="author" content="Marty Bonacci">
 		<meta name="description" content="About Marty Page"><!--The description appears with the title in search reaults.-->
 		<title>About Marty Page</title>
 	</head>
 	<body>
-		<h1>About Marty B</h1>
+		<h1>About Marty Bonacci</h1>
 		<img src="images/marty-on-mt-taylor.jpg">
 		<p>More about Marty</p>
 		<p></p>
