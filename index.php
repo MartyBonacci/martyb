3beff063cda08833f57892d71e986c06d8ba8b1b
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 15:45:48 -0600

    created about Marty B page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="About Marty Page"><!--The description appears with the title in search reaults.-->
+		<title>About Marty Page</title>
+	</head>
+	<body>
+		<h1>About Marty Bonacci</h1>
+		<img src="images/marty-on-mt-taylor.jpg">
+		<p>More about Marty</p>
+		<p></p>
+
+
+
+
+	</body>
+</html>
