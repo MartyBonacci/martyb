e6d302421fb23b95113012ebcfd78246e2cf3475
Marty Bonacci <martybonacci@gmail.com>
2017-07-10 20:10:24 -0600

    remove image

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
 		<meta name="author" content="Marty Bonacci">
 		<meta name="description" content="About Marty Page"><!--The description appears with the title in search reaults.-->
 		<title>About Marty Page</title>
-		<style>
-			body {background-image: url("images/marty-on-mt-taylor.jpg");}
-		</style>
+
 	</head>
 	<body>
 
